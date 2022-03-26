83ea4c1d1548c9cc2ceb5852c25590a4a56701f8
Bela Schaum <schaumb@users.noreply.github.com>
2022-03-26 23:14:51 +0100

    add streamlit

--- recognize.py
+++ recognize.py
@@ -1,22 +1,98 @@
+import datetime
+import functools
+import json
 import os
+import re
 import sys
 import requests
+import pandas as pd
+import streamlit as st
+from st_aggrid import AgGrid
+import requests
 
-if __name__ == '__main__':
-    while True:
-        line = sys.stdin.readline().strip().replace('ö', '0')
+st.title("RePharma admission form")
 
-        import requests
 
-        url = "https://google-search3.p.rapidapi.com/api/v1/search/q=site:https://pingvinpatika.hu%20" + line
+@st.experimental_singleton
+def get_pharma_list():
+    return pd.read_csv("data/tk_lista.csv")
 
-        headers = {
-            "X-User-Agent": "mobile",
-            "X-Proxy-Location": "EU",
-            "X-RapidAPI-Host": "google-search3.p.rapidapi.com",
-            "X-RapidAPI-Key": os.environ['API_KEY']
-        }
 
-        response = requests.request("GET", url, headers=headers)
+@st.experimental_singleton
+def get_registered_pharma_list():
+    return pd.read_csv("data/registered_pharma_list.csv")
 
-        print(response.json()['results'][0]['title'].split(' - ')[0].strip())
+
+@st.experimental_memo(show_spinner=False)
+def get_pharma_information(barcode):
+    url = "https://google-search3.p.rapidapi.com/api/v1/search/q=site:https://pingvinpatika.hu%20" + barcode
+
+    headers = {
+        "X-User-Agent": "mobile",
+        "X-Proxy-Location": "EU",
+        "X-RapidAPI-Host": "google-search3.p.rapidapi.com",
+        "X-RapidAPI-Key": st.secrets['API_KEY']
+    }
+
+    with st.spinner("Searching for the product..."):
+        response = requests.request("GET", url, headers=headers).json()['results']
+
+        if response:
+            return response[0]['title'].split(' - ')[0].strip(), \
+                   requests.request("GET", response[0]['link']).text.split('Kiszerelés:')[1].split('</div>')[0].split('>')[-1]
+
+        else:
+            url = "https://google-search3.p.rapidapi.com/api/v1/search/q=site:https://egeszsegpalace.hu%20" + barcode
+
+            response = requests.request("GET", url, headers=headers).json()['results']
+
+            if response:
+                return response[0]['title'].split(' - ')[0].strip(), \
+                       requests.request("GET", response[0]['link']).text.split('Kiszerelés: ')[1].split('<')[0].strip()
+            else:
+                return None, None
+
+
+df = get_registered_pharma_list()
+line_wr = st.empty()
+content = st.container()
+restart = st.empty()
+
+if restart.button("New product"):
+    st.session_state.ean = ""
+
+line = line_wr.text_input("EAN number", key="ean").strip().replace('ö', '0')
+
+with content:
+    if line:
+        line_wr.empty()
+        name, quantity = get_pharma_information(line)
+        if name:
+            st.header("Name")
+            st.write(name)
+            st.subheader("Quantity")
+            st.write(quantity)
+
+            max_quantity = int(functools.reduce(lambda i, j: float(i) * float(j), re.findall(r'[\d.]+', quantity)))
+
+            today = datetime.date.today()
+
+            with st.form(key="form"):
+                expiry_date = st.date_input("Expiry date", min_value=today)
+
+                quantity_c, unit_of_measurement_c = st.columns([0.75, 0.25])
+                left_quantity = quantity_c.selectbox("Quantity left in product", reversed(range(1, max_quantity+1)), key='quantity')
+                default_unit = 1 if 'ml' in quantity else 2 if 'g' in quantity else 0
+                unit_of_measurement_c.selectbox("Unit of measurement", ['piece(s)', 'ml', 'gram(s)'], index=default_unit, key='mertekegyseg')
+
+                if st.form_submit_button("Submit"):
+                    df.loc[len(df.index)] = [line, name, quantity, left_quantity, expiry_date.strftime("%m-%Y")]
+                    df.to_csv("data/registered_pharma_list.csv", index=False)
+                    st.success("Product has been added to the list!")
+
+        else:
+            st.warning("No information found")
+    else:
+        restart.empty()
+
+g = AgGrid(df.filter(['Name', 'Left quantity', 'Expiry date']).sort_values('Expiry date'), reload_data=True, fit_columns_on_grid_load=True, key='aggrid')
